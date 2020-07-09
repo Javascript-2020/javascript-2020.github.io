bf260c82aea32ca4b58e37864f7ba99d4c8e6dbc
Javascript-2020 <18556372079@163.com>
2020-07-09 17:59:20 +0800

    New5

--- innerJavaScript.js
+++ innerJavaScript.js
@@ -6,3 +6,7 @@ function videoTwo() {
     document.getElementById('video').src="https://apd-e3b6acf4474e9942181edd71ddc359db.v.smtcdns.com/mv.music.tc.qq.com/A74Z5QJ6uB6iYXsA1OTuArI98IDkgQraSMRrfG2iVpgg/14E161D6BD0493C7BE247810D2F170BB6ACF6F0F1FC124D7C90F6AC0B204BB0A70B65488B3BD1B698C9A344E5B785C50ZZqqmusic_default/qmmv_0a6bmyfgaabv2ribameakcqkb4fvbv3rvmpcglroiebaedqfbmda.f9844.mp4?fname=qmmv_0a6bmyfgaabv2ribameakcqkb4fvbv3rvmpcglroiebaedqfbmda.f9844.mp4";
     document.getElementById('text').value="无条件";
 }
+function videoThree() {
+    document.getElementById('video').src="https://apd-08a6eadb95833193eb453fab6c3efdf5.v.smtcdns.com/mv.music.tc.qq.com/APRA6NLG5sQSe9W6ylTfJwU-GnBh9lMu8jj5K7vlvrz4/0B0E150A2B5854FFC1A01637E5438E85C19537A4F53F94AB0CDE36A951354288A6FA12B5333C1DA9CC5E7CEAFB75803AZZqqmusic_default/qmmv_0a6bm7fgauhvyrqmayga4aagayavvuth7vyq6eidiagaccqgbqaq.f9824.mp4?fname=qmmv_0a6bm7fgauhvyrqmayga4aagayavvuth7vyq6eidiagaccqgbqaq.f9824.mp4";
+    document.getElementById('text').value="爱如潮水";
+}
