1ebd28c00a5c955177247b9cc062b01e2265959f
Javascript-2020 <18556372079@163.com>
2020-07-12 22:39:10 +0800

    Video configuation 2

--- innerJavaScript.js
+++ innerJavaScript.js
@@ -1,12 +1,12 @@
 function myFunction1() {
     document.getElementById('video').src="https://apd-0dc4ac76a18bca1cc618c2b43676c5aa.v.smtcdns.com/mv.music.tc.qq.com/AZtJuWwsh17omAsVc4uAUDsKjsc1A0kp5ZoXBW5q5EiI/69E5553FD82AD7B538E206D79996789B5A14CD35BF653A91464D7EAF5FFFBBA4F281170AB4541D104BF791443B3D9487ZZqqmusic_default/qmmv_0a6bu7crbabfmtycauda6aambecvdutjnclqbce3i4cakdqlamca.f9824.mp4?fname=qmmv_0a6bu7crbabfmtycauda6aambecvdutjnclqbce3i4cakdqlamca.f9824.mp4";
-    document.getElementById('text').value="无法阻挡";
+    document.getElementById('text').value="mu1";
 }
 function videoTwo() {
-    document.getElementById('video').src="https://apd-e3b6acf4474e9942181edd71ddc359db.v.smtcdns.com/mv.music.tc.qq.com/A74Z5QJ6uB6iYXsA1OTuArI98IDkgQraSMRrfG2iVpgg/14E161D6BD0493C7BE247810D2F170BB6ACF6F0F1FC124D7C90F6AC0B204BB0A70B65488B3BD1B698C9A344E5B785C50ZZqqmusic_default/qmmv_0a6bmyfgaabv2ribameakcqkb4fvbv3rvmpcglroiebaedqfbmda.f9844.mp4?fname=qmmv_0a6bmyfgaabv2ribameakcqkb4fvbv3rvmpcglroiebaedqfbmda.f9844.mp4";
-    document.getElementById('text').value="无条件";
+    document.getElementById('video').src="https://apd-c5e4b6f83ffbd666ba9dab5f653e8580.v.smtcdns.com/mv.music.tc.qq.com/Ax2dBSmUqC66KSoYLh0wcfFLkco1p_tL6KEhf9tX5XNI/F4607C031081BB0A38F2480396F3014E0BC7F745705E218089736691967C5B109226586F4DFFFE8124C0AF7C2143AF81ZZqqmusic_default/qmmv_0a6bnavmaqgfyrigamhqkbqnbebfrwmawrfgdie4iygqgbyhbmhq.f9844.mp4?fname=qmmv_0a6bnavmaqgfyrigamhqkbqnbebfrwmawrfgdie4iygqgbyhbmhq.f9844.mp4";
+    document.getElementById('text').value="mu2";
 }
 function videoThree() {
-    document.getElementById('video').src="https://apd-08a6eadb95833193eb453fab6c3efdf5.v.smtcdns.com/mv.music.tc.qq.com/APRA6NLG5sQSe9W6ylTfJwU-GnBh9lMu8jj5K7vlvrz4/0B0E150A2B5854FFC1A01637E5438E85C19537A4F53F94AB0CDE36A951354288A6FA12B5333C1DA9CC5E7CEAFB75803AZZqqmusic_default/qmmv_0a6bm7fgauhvyrqmayga4aagayavvuth7vyq6eidiagaccqgbqaq.f9824.mp4?fname=qmmv_0a6bm7fgauhvyrqmayga4aagayavvuth7vyq6eidiagaccqgbqaq.f9824.mp4";
-    document.getElementById('text').value="爱如潮水";
+    document.getElementById('video').src="https://apd-0ce75938a07c70b19af7f8f801c23d31.v.smtcdns.com/mv.music.tc.qq.com/A24S8Fe1SuDhBHoFZdC2TqgiSwfoQYBkh-63JXFHniLA/90384DF9A486FC6E7803F2892728C52A903D2FC2FFD5833FD767AC8AEF7C03E05FD7BCC7B233606BB0F1551401B19773ZZqqmusic_default/qmmv_0a6bmmioaicvwtqnamdaobakbyfvxw3kpzavvpjkieaq4baja4hq.f9824.mp4?fname=qmmv_0a6bmmioaicvwtqnamdaobakbyfvxw3kpzavvpjkieaq4baja4hq.f9824.mp4";
+    document.getElementById('text').value="mu3";
 }
